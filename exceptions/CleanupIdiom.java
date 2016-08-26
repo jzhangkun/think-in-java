87631a73eda5511a9d72c6a2973ce10d7af5c96a
jack.zhang <jack.zhang@bleum.com>
2016-08-26 12:10:03 +0800

    construction failed case

--- exceptions/CleanupIdiom.java
+++ exceptions/CleanupIdiom.java
@@ -4,15 +4,18 @@ package exceptions;
  * Created by jack.zhang on 8/25/2016.
  */
 class NeedsCleanup {
+    // Construcation can not fail
     private static long counter = 1;
     private final long id = counter++;
-    public NeedsCleanup() {
-    }
     public void dispose() {
         System.out.println("NeedsCleanup " + id + " disposed");
     }
 }
 class ConstructionException extends Exception {}
+class NeedsCleanup2 extends NeedsCleanup {
+    // Construction can fail:
+    public NeedsCleanup2() throws ConstructionException {}
+}
 public class CleanupIdiom {
     public static void main(String[] args) {
         // Section 1:
@@ -36,9 +39,9 @@ public class CleanupIdiom {
         // Section 3:
         // If construction can fail, you must guard each one:
         try {
-            NeedsCleanup nc4 = new NeedsCleanup();
+            NeedsCleanup nc4 = new NeedsCleanup2();
             try {
-                NeedsCleanup nc5 = new NeedsCleanup();
+                NeedsCleanup nc5 = new NeedsCleanup2();
                 try {
                     //...
                 } finally {
